50e55098827b8518751ebb186b505707561171ea
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-01 15:57:54 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,21 @@
-# Remove this comment to confirm that this task is done
+def fibonacci(n):
+    if n < 0:
+        print("error")
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+
+def fibonacci_sequence(n):
+    result=[]
+    while len(result) != n:
+        for i in range(n):
+            result.insert(0,fibonacci(n-i-1) )
+
+    return result
+print(fibonacci_sequence(6))
