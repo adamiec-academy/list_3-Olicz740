8e49e1c71008bbf412374f7b20bb5fbf28bdc804
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-14 18:07:23 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,12 @@
 def get_perfect_numbers(n):
-    return []
+    k = 10
+    divisors = [1]
+    result = []
+    for i in range(2, k):
+        if (k % i) == 0:
+            divisors.append(i)
+    for i in range(k):
+        if i == sum(divisors) and len(result) < n:
+            result.append(i)
+    return result
+print(get_perfect_numbers(2))
