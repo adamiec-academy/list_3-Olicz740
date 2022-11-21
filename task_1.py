fab559e15da548434a78c664949f9d3d16f35600
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-21 21:01:56 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,12 +1,22 @@
+def is_perfect(k):
+    dividors = []
+    for i in range(1, k):
+        if k % i == 0:
+            dividors.append(i)
+    if sum(dividors) == k:
+        return True
+    else:
+       return False
+
 def get_perfect_numbers(n):
-    k = 10
-    divisors = [1]
+
+    k = 100
     result = []
-    for i in range(2, k):
-        if (k % i) == 0:
-            divisors.append(i)
     for i in range(k):
-        if i == sum(divisors) and len(result) < n:
+        if is_perfect(k) is True and len(result) < n:
             result.append(i)
+        else:
+            k += 1
+
     return result
 print(get_perfect_numbers(2))
